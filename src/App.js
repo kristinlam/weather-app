bce88b65fa8e890e6df034a603848e68d82e12ff
Kristin Lam <klam623@gmail.com>
2022-10-05 15:08:32 -0400

    Add city and image to App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// const URL = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${process.env.REACT_APP_API_KEY}`;
+const URL = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=fe401b4fb11016097b8ab6892da9c0a4;`;
 
 function App() {
+  console.log('the url is:', URL);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="city">
+        <img
+          style={{ maxWidth: 200 }}
+          className="city__image"
+          alt="Fuzhou"
+          src={require(`./images/fuzhou.jpg`)}
+        />
+        <div className="city__info">
+          <p className="city__info--name">Fuzhou</p>
+          <p className="city__info--temp">90 degrees</p>
+        </div>
+      </div>
     </div>
   );
 }

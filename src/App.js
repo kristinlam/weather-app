ccb00e3b545b50613e4f27fcc976f76f733716a0
Kristin Lam <klam623@gmail.com>
2022-10-06 11:49:06 -0400

    Use City component inside App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-import axios from 'axios';
+import City from './City';
 
-const searchCity = (city) => {
-  axios
-    .get(
-      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=fe401b4fb11016097b8ab6892da9c0a4`
-    )
-    .then((res) => {
-      console.log(res.data);
-    });
-};
-
-const citiesArray = ['Fuzhou', 'Hong Kong', 'New York'];
-
-citiesArray.forEach(searchCity);
+const citiesArray = ['Fuzhou, CN', 'Hong Kong, CN', 'New York, US'];
 
 function App() {
   return (
-    <div className="app">
-      <div className="city">
-        <img
-          style={{ maxWidth: 200 }}
-          className="city__image"
-          alt="Fuzhou"
-          src={require(`./images/fuzhou.jpg`)}
-        />
-        <div className="city__info">
-          <p className="city__info--name">Fuzhou</p>
-          <p className="city__info--temp">90 degrees</p>
-        </div>
-      </div>
+    <div className="app" style={{ display: 'flex' }}>
+      {citiesArray.map((el, idx) => (
+        <City name={el} key={idx} />
+      ))}
     </div>
   );
 }

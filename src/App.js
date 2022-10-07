22bf0c192fbf6343db8493715a034dc3e3bb6251
Kristin Lam <klam623@gmail.com>
2022-10-07 14:37:24 -0400

    Refactor to make API calls in App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import City from './City';
+import Modal from './Modal';
 import styles from './styles/app.module.css';
 
-const citiesArray = ['Fuzhou, CN', 'Hong Kong, CN', 'New York, US'];
+const cities = ['Fuzhou, CN', 'Hong Kong, CN', 'New York, US'];
 
 function App() {
+  const [citiesWeather, setCitiesWeather] = useState(null);
+  const [modal, setModal] = useState({ show: false, data: null });
+
+  useEffect(() => {
+    const getWeatherData = () => {
+      let promises = cities.map((city) =>
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe401b4fb11016097b8ab6892da9c0a4&units=imperial`
+        )
+      );
+
+      Promise.all(promises).then((responses) => {
+        const weatherData = responses.map((res) => res.data);
+        console.log(weatherData);
+        setCitiesWeather(weatherData);
+      });
+    };
+
+    getWeatherData();
+  }, []);
+
+  const openModal = (idx) => {
+    setModal({ show: true, data: citiesWeather[idx] });
+  };
+
+  const closeModal = () => {
+    setModal({ show: false, data: null });
+  };
+
   return (
-    <div className="app">
-      <h1 className={styles.title}>Weather App</h1>
+    <div className={styles.app}>
+      <div className={styles.heading}>
+        <h1 className={styles.title}>Weather App</h1>
+        <h1 className={styles.subtitle}>Short description to go here</h1>
+      </div>
       <div className={styles.home}>
-        {citiesArray.map((el, idx) => (
-          <City name={el} key={idx} />
+        {citiesWeather?.map((el, idx) => (
+          <City data={el} key={idx} idx={idx} openModal={openModal} />
         ))}
       </div>
+      {modal.show && modal.data && (
+        <div>
+          <Modal closeModal={closeModal} data={modal.data} />
+          <div className={styles.overlay} onClick={closeModal}></div>
+        </div>
+      )}
     </div>
   );
 }
--- src/City.js
+++ src/City.js
@@ -1,58 +1,19 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Modal from './Modal';
 import styles from './styles/city.module.css';
 
-const City = ({ name }) => {
-  const [city, setCity] = useState(null);
-  const [modal, setModal] = useState({ show: false, data: null });
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=fe401b4fb11016097b8ab6892da9c0a4&units=imperial`
-      )
-      .then((res) => {
-        setCity(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  const openModal = () => {
-    console.log('open modal city', city);
-    setModal({ show: true, data: city });
-  };
-
-  const closeModal = () => {
-    setModal({ show: false, data: null });
-    console.log('close modal city', city);
-  };
-
+const City = ({ openModal, idx, data }) => {
   return (
-    <div className="container">
-      <div className="city" onClick={openModal}>
-        {city ? (
-          <div>
-            <img
-              className={styles.city__image}
-              alt={city.name}
-              src={require(`./images/${city.name}.jpg`)}
-            />
-
-            <div className="city__info">
-              <h2 className="city__info--name">{city.name}</h2>
-              <p className="city__info--temp">
-                {Math.round(city.main.temp * 10) / 10}°F
-              </p>
-            </div>
-          </div>
-        ) : null}
+    <div className={styles.city} onClick={() => openModal(idx)}>
+      <img
+        className={styles.city__image}
+        alt={data.name}
+        src={require(`./images/${data.name}.jpg`)}
+      />
+      <div className={styles.city__info}>
+        <h2 className={styles.city__info__name}>{data.name}</h2>
+        <p className={styles.city__info__temp}>
+          {Math.round(data.main.temp * 10) / 10}°F
+        </p>
       </div>
-      {modal.show && modal.data && (
-        <Modal closeModal={closeModal} data={modal.data} />
-      )}
     </div>
   );
 };

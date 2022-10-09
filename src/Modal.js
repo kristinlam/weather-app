cc5d44c0292df2e5963e9c26881250d6a83cefe9
Kristin Lam <klam623@gmail.com>
2022-10-08 22:17:45 -0400

    Style modal

--- src/Modal.js
+++ src/Modal.js
@@ -4,21 +4,47 @@ const Modal = ({ closeModal, data }) => {
   const weather = data.weather[0];
 
   return (
-    <div className={styles.modal}>
-      <button className={styles.btn__close} onClick={closeModal}>
-        X
-      </button>
-      <h2>{data.name}</h2>
-      <p>
-        {weather.description.charAt(0).toUpperCase() +
-          weather.description.slice(1)}
-      </p>
-      <p>{Math.round(data.main.temp * 10) / 10}°F</p>
-      <p>
-        Atmospheric pressure: {data.main.pressure.toLocaleString('en-US')} hPa
-      </p>
-      <p>Humidity: {data.main.humidity}%</p>
-      <p>Visibility: {data.visibility.toLocaleString('en-US')} m</p>
+    <div className={`${styles.modal} ${styles.row}`}>
+      <div className={`${styles.image__container} ${styles.column}`}>
+        <img alt={data.name} src={require(`./images/${data.name}.jpg`)} />
+        <a href="https://unsplash.com/@guphotos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Thana Gu
+        </a>
+      </div>
+      <div className={`${styles.text__container} ${styles.column}`}>
+        <button className={styles.btn__close} onClick={closeModal}>
+          X
+        </button>
+
+        <div className={styles.weather}>
+          <h2>{data.name}</h2>
+          <div>
+            <p>
+              {weather.description.charAt(0).toUpperCase() +
+                weather.description.slice(1)}
+            </p>
+
+            <p>{Math.round(data.main.temp * 10) / 10}°F</p>
+          </div>
+
+          <div>
+            <div>
+              <h3>Atmospheric pressure</h3>
+              <p>{data.main.pressure.toLocaleString('en-US')} hPa</p>
+            </div>
+
+            <div>
+              <h3>Humidity</h3>
+              <p>{data.main.humidity}%</p>
+            </div>
+
+            <div>
+              <h3>Visibility</h3>
+              <p>{data.visibility.toLocaleString('en-US')} m</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

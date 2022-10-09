6a2741e3b57fff89927fb5bf14cca4ca89303733
Kristin Lam <klam623@gmail.com>
2022-10-09 17:10:04 -0400

    Update cities information

--- src/cities.js
+++ src/cities.js
@@ -1,11 +1,77 @@
 const cities = [
   {
+    name: 'Hong Kong',
+    photographer: 'Manson Yim',
+    photographerHandle: '@mansonyms',
+    photoDescription:
+      'Cityscape of the Victoria Harbour region of Hong Kong during a magical sunset.',
+    imageFile: 'hongkong.jpg',
+  },
+  {
     name: 'Bangkok',
     photographer: 'Nomadic Julien',
     photographerHandle: '@nomadicjulien',
     photoDescription:
       'Phra Maha Chedi Si Rajakarn, a group of 4 large pagodas in Wat Pho, one of the oldest Buddhist temples in Bangkok, Thailand.',
     imageFile: 'bangkok.jpg',
+  },
+  {
+    name: 'Macau',
+    photographer: 'Simon Zhu',
+    photographerHandle: '@smnzhu',
+    photoDescription:
+      'A view of Macau (Portguese spelling, with Macao being the English spelling) and its iconic Grand Lisboa Hotel towering in the background.',
+    imageFile: 'macau.jpg',
+  },
+  {
+    name: 'Singapore',
+    photographer: 'Sreehari Devadas',
+    photographerHandle: '@sreeharid1',
+    photoDescription:
+      'Flower Dome and Cloud Forest conservatories in Gardens by the Bay, a nature park and Singapore\'s effort to create a "City in the Garden."',
+    imageFile: 'singapore.jpg',
+  },
+  {
+    name: 'London',
+    photographer: 'Alex Azabache',
+    photographerHandle: '@alexazabache',
+    photoDescription:
+      'Big Ben, a tower clock located in the Palace of Westminster in London, England.',
+    imageFile: 'london.jpg',
+  },
+  {
+    name: 'Paris',
+    photographer: 'Anthony Delanoix',
+    photographerHandle: '@anthonydelanoix',
+    photoDescription:
+      'The Eiffel Tower in Paris France, built to commemorate the centennial of the French Revolution.',
+    imageFile: 'paris.jpg',
+  },
+
+  {
+    name: 'Dubai',
+    photographer: 'ZQ Lee',
+    photographerHandle: '@zqlee',
+    photoDescription:
+      'The Burj Khalifa in Dubai, United Arab Emirates, the tallest structure and building in the world since 2009.',
+    imageFile: 'dubai.jpg',
+  },
+
+  {
+    name: 'Delhi',
+    photographer: 'Darklabs India',
+    photographerHandle: '@darklabsindia',
+    photoDescription:
+      'The Safdarjung Tomb in Delhi, India, the last monumental tomb garden of the Mughal Empire.',
+    imageFile: 'delhi.jpg',
+  },
+  {
+    name: 'Istanbul',
+    photographer: 'Meriç Dağlı',
+    photographerHandle: '@meric',
+    photoDescription:
+      'An early morning view of Bosphorus Bridge and Buyuk Mecidiye Mosque in Istanbul, Turkey.',
+    imageFile: 'istanbul.jpg',
   },
   {
     name: 'Kuala Lumpur',

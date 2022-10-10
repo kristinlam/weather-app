40ff5643ebc3f8869adaaec4f8ef54b9d3f111b3
Kristin Lam <klam623@gmail.com>
2022-10-09 20:07:03 -0400

    Move logic in App into Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import CityCard from './components/CityCard';
-import CityModal from './components/CityModal';
-import styles from './styles/app.module.css';
-import modalStyles from './styles/CityModal.module.css';
-import cities from './cities.js';
+import Dashboard from './components/Dashboard';
 
 function App() {
-  const [citiesWeather, setCitiesWeather] = useState(null);
-  const [modal, setModal] = useState({ show: false, data: null });
-
-  useEffect(() => {
-    const getWeatherData = () => {
-      let promises = cities.map((city) =>
-        axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city.name}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`
-        )
-      );
-
-      Promise.all(promises).then((responses) => {
-        const weatherData = responses.map((res) => res.data);
-        console.log(weatherData);
-        setCitiesWeather(weatherData);
-      });
-    };
-
-    getWeatherData();
-  }, []);
-
-  const openModal = (index) => {
-    setModal({ show: true, index: index, data: citiesWeather[index] });
-  };
-
-  const closeModal = () => {
-    setModal({ show: false, index: null, data: null });
-  };
-
   return (
-    <div className={styles.app}>
-      <div className={styles.heading}>
-        <h1 className={styles.title}>Weather App</h1>
-        <div className={styles.description}>
+    <div className="app">
+      <div className="heading">
+        <h1 className="title">Weather App</h1>
+        <div className="description">
           <p>Today's weather for the top 10 most visited cities</p>
           <p>
             Weather data from OpenWeatherMap API, city data from Bloomberg, and
@@ -48,21 +13,8 @@ function App() {
           </p>
         </div>
       </div>
-      <div className={styles.home}>
-        {citiesWeather?.map((el, i) => (
-          <CityCard key={i} index={i} data={el} openModal={openModal} />
-        ))}
-      </div>
-      {modal.show && modal.data && (
-        <div className={modalStyles.container}>
-          <CityModal
-            index={modal.index}
-            data={modal.data}
-            closeModal={closeModal}
-          />
-          <div className={modalStyles.overlay} onClick={closeModal}></div>
-        </div>
-      )}
+
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import CityCard from './CityCard';
+import CityModal from './CityModal';
+import styles from '../styles/Dashboard.module.css';
+import modalStyles from '../styles/CityModal.module.css';
+import cities from '../cities.js';
+
+const Dashboard = () => {
+  const [citiesWeather, setCitiesWeather] = useState(null);
+  const [modal, setModal] = useState({ show: false, data: null });
+
+  useEffect(() => {
+    const getWeatherData = () => {
+      let promises = cities.map((city) =>
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city.name}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`
+        )
+      );
+
+      Promise.all(promises).then((responses) => {
+        const weatherData = responses.map((res) => res.data);
+        console.log(weatherData);
+        setCitiesWeather(weatherData);
+      });
+    };
+
+    getWeatherData();
+  }, []);
+
+  const openModal = (index) => {
+    setModal({ show: true, index: index, data: citiesWeather[index] });
+  };
+
+  const closeModal = () => {
+    setModal({ show: false, index: null, data: null });
+  };
+
+  return (
+    <div>
+      <div className={styles.dashboard}>
+        {citiesWeather?.map((el, i) => (
+          <CityCard key={i} index={i} data={el} openModal={openModal} />
+        ))}
+      </div>
+      {modal.show && modal.data && (
+        <div className={modalStyles.container}>
+          <CityModal
+            index={modal.index}
+            data={modal.data}
+            closeModal={closeModal}
+          />
+          <div className={modalStyles.overlay} onClick={closeModal}></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
